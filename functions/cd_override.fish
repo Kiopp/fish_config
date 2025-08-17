02e6b05bf9c707591e32f16ced8d59eb3e502885
Jesper Wentzell <jesper.wentzell@gmail.com>
2025-08-18 01:18:03 +0200

    Added cd_override function to always execute ls efter using cd

--- functions/cd_override.fish
+++ functions/cd_override.fish
@@ -1,5 +1,4 @@
-# ~/.config/fish/functions/fish_postexec.fish
-function fish_postexec
+function cd_override --on-event fish_postexec
   # Check if the last command was 'cd'
   if test "$argv[1]" = "cd"
     # Check if the last command exited successfully (status 0)
