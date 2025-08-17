ef78f6ce630d57fd15e7fcf97963276c4f492d30
Jesper Wentzell <jesper.wentzell@gmail.com>
2025-08-18 00:55:28 +0200

    Initial commit
    Initial configurations commit

---
+++ config.fish
@@ -0,0 +1,29 @@
+# Add ~/.local/bin to PATH (for oh-my-posh)
+set -gx PATH $PATH /home/jesper/.local/bin
+
+# Oh My Posh prompt configuration
+oh-my-posh init fish --config $HOME/.config/ohmyposh/EDM115-newline.omp.json | source
+
+# Auto-start fastfetch
+if tty | string match "*pts*" > /dev/null;
+    fastfetch
+end
+
+# -----------------------------------------------------
+# System aliases
+# -----------------------------------------------------
+alias ls='eza -a --icons=always'
+alias ll='eza -al --icons=always'
+alias lt='eza -a --tree --level=1 --icons=always'
+alias shutdown='systemctl poweroff'
+alias m='micro'
+alias c='clear'
+
+# -----------------------------------------------------
+# Git aliases
+# -----------------------------------------------------
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+alias gpl="git pull"
---
+++ functions/fish_postexec.fish
@@ -0,0 +1,11 @@
+# ~/.config/fish/functions/fish_postexec.fish
+function fish_postexec
+  # Check if the last command was 'cd'
+  if test "$argv[1]" = "cd"
+    # Check if the last command exited successfully (status 0)
+    if test $status -eq 0
+      # List the contents of the new directory
+      ls
+    end
+  end
+end
